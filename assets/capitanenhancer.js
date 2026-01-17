93e5feb12bf48fe006e0d1cd02d4383a31dc656a
NMTYP <jonathancsv@gmail.com>
2026-01-17 23:10:28 +0100

    Update capitanenhancer.js
    correct group ID

--- assets/capitanenhancer.js
+++ assets/capitanenhancer.js
@@ -1,4 +1,3 @@
 ﻿// https://github.com/mcmonkeyprojects/SwarmUI/blob/b6c6b7377b25fd820516648215b724a8ca63037f/src/wwwroot/js/genpage/gentab/params.js#L1474
 // addInstallButton(groupId, featureId, installId, buttonText)
-addInstallButton('capitanenhancer', 'capitan_enhancer', 'capitan_enhancer', 'Install Capitan Conditioning Enhancer');
-console.log(" ################################################## Attempted to add button ##################################################");
+addInstallButton('conditioningenhancer', 'capitan_enhancer', 'capitan_enhancer', 'Install Capitan Conditioning Enhancer');
